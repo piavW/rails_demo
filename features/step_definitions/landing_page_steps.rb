6bae1e81d406afb9b266d58d23b59b13c36b3441
piavw <piavonwachenfelt@hotmail.com>
2019-09-23 15:43:54 +0200

    Updates stepdefinition and adds backgroundblock referencing articles in database.

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,3 +1,9 @@
+Given(/^the following articles exists$/) do |table|
+    table.hashes.each do |hash|
+      Article.create!(hash)
+    end
+  end
+
 When(/^I am on the landing page$/) do
     visit root_path
   end

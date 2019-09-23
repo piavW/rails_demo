0f4b645bead3539bb5649fd28a66c91ef635bac2
piavw <piavonwachenfelt@hotmail.com>
2019-09-23 16:48:57 +0200

    Updates step-definitions to have a link on the page.

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -3,7 +3,7 @@ visit root_path
 end
 
 When("I click {string} link") do |string|
-pending # Write code here that turns the phrase above into concrete actions
+    expect(page).to have_link link
 end
 
 When("I fill in {string} with {string}") do |string, string2|

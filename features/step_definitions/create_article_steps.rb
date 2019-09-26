475064100857e82918e9973e90ad02bf45478fe9
piavw <piavonwachenfelt@hotmail.com>
2019-09-26 16:15:29 +0200

    Modifies feature-steps for flash notice
    - Rewrites steps from visit... to expect current_path to be...
    - Moves div with flash notice from show.html.erb to layouts application.

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -11,7 +11,8 @@ When("I fill in {string} with {string}") do |string, string2|
 end
 
 Then("I should be on {string} page") do |string|
-    visit articles_path
+    article = Article.find_by(title: string)
+    expect(current_path).to eq article_path(article) 
 end
 
 When("I should see {string}") do |string|

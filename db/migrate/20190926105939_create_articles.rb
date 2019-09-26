078f9a9ee1872ba39d74aae304b553ce083bfcef
piavw <piavonwachenfelt@hotmail.com>
2019-09-26 13:25:12 +0200

    Creates article controller and funtionality
    - Creates article-controller
    - Creates article modell
    - Creates article-pages: index, new and show.
    - Configures routes to link from landingpage to article page

--- db/migrate/20190926105939_create_articles.rb
+++ db/migrate/20190926105939_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :content
+      t.text :text
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_073948) do
+ActiveRecord::Schema.define(version: 2019_09_26_105939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "links", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

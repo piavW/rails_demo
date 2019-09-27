76f1dfd5c34e554ed8cc9c2aa66a5f3265d5245f
piavw <piavonwachenfelt@hotmail.com>
2019-09-27 19:10:42 +0200

    Removes description of name from rspec User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :id }
-    it { is_expected.to have_db_column :name }
     it { is_expected.to have_db_column :email }
     it { is_expected.to have_db_column :encrypted_password }
 end 
 
 describe 'Validations' do
-    it {is_expected.to validate_presence_of :name}
     it {is_expected.to validate_presence_of :email}
 end
 

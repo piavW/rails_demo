e4ab320917d8376974fe8deca42c3719ad878b60
piavw <piavonwachenfelt@hotmail.com>
2019-09-27 12:17:40 +0200

    Reverts to previous root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'landing/index'
-  # root controller: :landing, action: :index 
-  root controller: :articles, action: :index
+  root controller: :landing, action: :index 
   resources :articles
 end

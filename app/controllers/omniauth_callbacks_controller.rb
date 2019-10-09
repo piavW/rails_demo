6f78b69615048158319cd58ad84e8c574b5ef016
oliverochman <oliver.ochman@hotmail.com>
2019-10-09 11:43:42 +0200

    add provider & uid to user model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted? 
+    if @user.persisted?    
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
